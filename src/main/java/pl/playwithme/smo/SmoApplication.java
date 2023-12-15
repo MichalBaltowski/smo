af76f19ad2b73db7ef0227429b202517a5599d26
MichalBaltowski <mbaltowski@gmail.com>
2023-12-15 21:31:30 +0100

    new vnaila project, with changed name with fixed pom.xml

---
+++ src/main/java/pl/playwithme/smo/SmoApplication.java
@@ -0,0 +1,13 @@
+package pl.playwithme.smo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoApplication.class, args);
+	}
+
+}
