f04737d721f6611cea410357be31a51052dd671a
MichalBaltowski <mbaltowski@gmail.com>
2023-12-16 11:25:04 +0100

    Added patch, delete, post for controlling users on db

--- src/main/java/pl/playwithme/smo/Database/DBRepository.java
+++ src/main/java/pl/playwithme/smo/Database/DBRepository.java
@@ -1,7 +1,7 @@
 package pl.playwithme.smo.Database;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -19,5 +19,49 @@ public class DBRepository {
         List<User> query = jdbcTemplate.query("SELECT * FROM user",
                 BeanPropertyRowMapper.newInstance(User.class));
         return query;
+    }
+
+    public User getById(int id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM user WHERE id = ?",
+                BeanPropertyRowMapper.newInstance(User.class), id);
+    }
+
+    public boolean save(List<User> users) {
+        try {
+            users.forEach(user -> jdbcTemplate.update("INSERT INTO user (name, password) VALUES (?,?)",
+                    user.getName(), user.getPassword()));
+        } catch (DataAccessException exception) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean update(int id, User user) {
+        var userToUpdate = getById(id);
+        if (userToUpdate != null) {
+            if (userToUpdate.getName() != null) {
+                userToUpdate.setName(user.getName());
+            }
+            if (userToUpdate.getPassword() != null) {
+                userToUpdate.setPassword(user.getPassword());
+            }
+            jdbcTemplate.update("UPDATE user SET name=?,password=? WHERE id =?",
+                    userToUpdate.getName(),
+                    userToUpdate.getPassword(),
+                    userToUpdate.getId());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean delete(int id) {
+        var userToDelete = getById(id);
+        if (userToDelete != null) {
+            jdbcTemplate.update("DELETE FROM user WHERE id = ?", id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/pl/playwithme/smo/UserController.java
+++ src/main/java/pl/playwithme/smo/UserController.java
@@ -1,27 +1,42 @@
 package pl.playwithme.smo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.playwithme.smo.DBEntity.User;
 import pl.playwithme.smo.Database.DBRepository;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     DBRepository dbRepository;
 
-    @GetMapping("/user")
+    @GetMapping()
     public List<User> getAll() {
         return dbRepository.getAll();
     }
 
-    @GetMapping("/test")
-    public String getUser() {
-        return "TEST";
+    @GetMapping("/{id}")
+    public User getById(@PathVariable("id") int id) {
+        return dbRepository.getById(id);
     }
 
+    @PostMapping()
+    public boolean add(@RequestBody List<User> users) {
+        return dbRepository.save(users);
+    }
+
+    @PatchMapping("/{id}")
+    public boolean update(@PathVariable int id,
+                          @RequestBody User user) {
+        return dbRepository.update(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable int id) {
+       return dbRepository.delete(id);
+    }
 }
