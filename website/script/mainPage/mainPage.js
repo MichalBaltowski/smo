5670b37e3daca34aafd5c7fa65d2102ad72a0246
MichalBaltowski <mbaltowski@gmail.com>
2023-12-17 22:33:28 +0100

    created separated js and css filed per pages nad created common styles file

---
+++ website/script/mainPage/mainPage.js
@@ -0,0 +1,11 @@
+function showCalendar() {
+    window.location.href = 'calendarPage.html';
+}
+
+function showSettings() {
+    window.location.href = 'settingsPage.html';
+}
+
+function logOut() {
+    window.location.href = 'loginPage.html';
+}
